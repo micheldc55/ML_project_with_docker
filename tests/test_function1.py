bf9386f135fa8a9f7dd0a9c478b8e21e8b7ef7b2
Davidovich, Michel <davidovichm@ryanair.com>
2023-09-22 20:07:15 +0200

    added all folders and base files

---
+++ tests/test_function1.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestModuleToTest(unittest.TestCase):
+    def test_function_1(self):
+        # Your test code here
+        pass
+
+    def test_function_2(self):
+        # Your test code here
+        pass
