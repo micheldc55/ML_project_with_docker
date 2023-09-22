968be0cec2f02ad7d4c159d46b7d92eaf785baed
Davidovich, Michel <davidovichm@ryanair.com>
2023-09-22 17:46:34 +0000

    test precommit v2

---
+++ src/libs/test_precommit.py
@@ -0,0 +1,13 @@
+import logging
+import os
+import sys
+from os.path import samefile
+
+import numpy as np
+import pandas as pd
+from matplotlib.pyplot import figure
+
+
+def test_precom(a):
+    """Empty func"""
+    return a + 1
